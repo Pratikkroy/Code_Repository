bd695265ced54dc083698503b0f74af7b89210d4
pratik-ad <pratik.r@altradimension.com>
2019-06-30 13:23:45 +0530

    added BufferedReader

--- utilities/FastIO.java
+++ utilities/FastIO.java
@@ -1,15 +1,20 @@
 package utilities;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class FastIO {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int t= Integer.parseInt((in.readLine().trim()));
 		
 	}
 
